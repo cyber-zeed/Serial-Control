f7730407b80e6bb8f611b616edf2b1911c70514d
cyber-zeed <34373499+cyber-zeed@users.noreply.github.com>
2024-01-16 18:34:38 +0000

    Update serialcontrol.ino

--- serialcontrol.ino
+++ serialcontrol.ino
@@ -1,3 +1,38 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+
+const int numPins = 11;  // Number of GPIO pins available on ESP8266
+int pins[numPins] = {0, 2, 4, 5, 12, 13, 14, 15, 16};  // GPIO pins to control
+
+ESP8266WebServer server(80);
+
+const int EEPROM_SIZE = 512;  // EEPROM size on ESP8266
+
+void setup() {
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  connectToWiFi();
+
+  for (int i = 0; i < numPins; i++) {
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], LOW);
+  }
+
+  // Setup web server routes
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/on", HTTP_GET, handleOn);
+  server.on("/off", HTTP_GET, handleOff);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
 void connectToWiFi() {
   Serial.println("\nScanning for available Wi-Fi networks...");
 
@@ -58,4 +93,30 @@ void connectToWiFi() {
       }
     }
   }
+}
+
+void handleRoot() {
+  String html = "<html><body>";
+  html += "<h1>ESP8266 Web Server</h1>";
+  html += "<p>Click the buttons to control the ports:</p>";
+  
+  for (int i = 0; i < numPins; i++) {
+    html += "<button onclick=\"window.location.href='/on?pin=" + String(pins[i]) + "'\">Port " + String(pins[i]) + " On</button>";
+    html += "<button onclick=\"window.location.href='/off?pin=" + String(pins[i]) + "'\">Port " + String(pins[i]) + " Off</button><br>";
+  }
+
+  html += "</body></html>";
+  server.send(200, "text/html", html);
+}
+
+void handleOn() {
+  String pin = server.arg("pin");
+  digitalWrite(pin.toInt(), HIGH);
+  server.send(200, "text/plain", "Port " + pin + " turned ON");
+}
+
+void handleOff() {
+  String pin = server.arg("pin");
+  digitalWrite(pin.toInt(), LOW);
+  server.send(200, "text/plain", "Port " + pin + " turned OFF");
 }
